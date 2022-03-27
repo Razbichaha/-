5ba50bf378fdfa1005583590368d625a65bd5af7
Сергей <seregabur@live.ru>
2022-03-27 22:45:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Сумма_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte startNumberRandom = 10;
+            byte stopNumberRandom = 100;
+
+            Random Rnd = new Random();
+
+            int numberRandom = Rnd.Next(startNumberRandom, stopNumberRandom);
+            int multipleNumber1 = 3, multipleNumber2 = 5;
+            int summandNumber=0;
+
+            for(int i=0;i<=numberRandom;i++)
+            {
+                if(i%multipleNumber1==0|i%multipleNumber2==0)
+                {
+                    summandNumber = summandNumber + i;
+                }
+            }
+            Console.WriteLine("Сумма чисел кратных 3 и 5 = " + summandNumber);
+        }
+    }
+}
